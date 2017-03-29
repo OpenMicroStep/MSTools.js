642c0d81623f07ba18f7c3bd79d377d3c81de9de
Vincent Rouillé <v-rouille@logitud.fr>
2017-03-29 11:06:20 +0200

    remove dist/ from git

--- dist/types/mscouple.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class MSCouple<T1, T2> {
-    firstMember: T1 | null;
-    secondMember: T2 | null;
-    constructor(first?: T1 | null, second?: T2 | null);
-    toArray(): (T1 | T2 | null)[];
-    isEqualTo(other: any): boolean;
-    isEqualToCouple(other: MSCouple<T1, T2>): boolean;
-}
